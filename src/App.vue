abee1d6304ccff33a53d4ea54b07b9aabc05a971
Cameron Cooper <cameron.w.cooper@gmail.com>
2019-08-26 14:30:46 -0600

    added background image and ext color

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,11 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
   margin-top: 60px;
+}
+
+body {
+  background: url("./assets/background-min.jpg");
 }
 </style>
