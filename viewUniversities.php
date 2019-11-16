85f95ae31f49fda0d7ddfd234efce288fd48e278
sydneyfowler <42590145+sydneyfowler@users.noreply.github.com>
2019-11-16 12:40:43 -0700

    Add files via upload

---
+++ viewUniversities.php
@@ -0,0 +1,65 @@
+<?php 
+    include("./php/db.php");
+    include("./partials/header.html");
+?>
+
+<div class="pusher">
+    <div class="ui inverted vertical masthead center aligned segment">
+
+        <div class="ui container">
+            <div class="ui large secondary inverted pointing menu">
+                <h3><a href="/~group5">Rate The Losers</a></h3>
+
+                <div class="right item">
+                    <?php
+                    if (isset($_COOKIE["email"])) {
+                        echo '<a href="php/logout.php" class="ui inverted button">Log out</a>';
+                    } else {
+                        echo '<a href="login.html" class="ui inverted button">Log in</a>';
+                        echo '<a href="register.html" class="ui inverted button">Sign Up</a>';
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="ui search" id="universitySearch">
+            <label>University</label>
+            <input class="prompt" type="text"  name="q" placeholder="University of Colorado Colorado Springs">
+            <input class="ui green button" type="submit" name="submit" value="Find">
+        </div>
+        <table class="ui celled table">
+            <tbody>
+
+            <?php
+                // Form database query
+                if (isset($_POST['submit'])) {
+                    $q = $connection->real_escape_string($_POST['q']);
+                    $sql = "SELECT * FROM university WHERE name LIKE '%$q%'";
+                    
+                    $res = $conn->query($sql);
+
+                    if ($res->num_rows > 0) {
+                        // output data of each row
+                        while($row = $res->fetch_assoc()) {
+                            echo "<tr>
+                                    <td data-label='Name'>".$row["name"]."</td>
+                                </tr>";
+                            echo "<script>
+                                $('#".$row['name'].$row['lName']."').rating('disable');
+                            </script>";
+                        }
+                    } else {
+                        echo "0 results";
+                    }
+                }
+            ?>
+            </tbody>
+        </table>
+    </div>
+</div> 
+<?php include("./partials/footer.html"); ?>
+
+
+
+
