5fd816739d8e441cf25346760687da9342e35d89
Trentin <craishot@gmail.com>
2019-12-06 10:59:50 -0700

    Add primary key to losers table

--- php/submitLoser.php
+++ php/submitLoser.php
@@ -1,11 +1,12 @@
 <?php
     include("db.php");
 
+    $loser_id=uniqid();
     $firstName = $_POST["fName"];
     $lastName = $_POST["lName"];
     $university = $_POST["university"];
 
-    $sql = "INSERT INTO loser (fName, lName, university, rating) VALUES ('$firstName', '$lastName', '$university', '1')";
+    $sql = "INSERT INTO loser (loser_id, fName, lName, university, rating) VALUES ('$loser_id, $firstName', '$lastName', '$university', '1')";
     $res = $conn->query($sql);
     header('Location: /~group5');
 ?>
